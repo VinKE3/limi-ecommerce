add02e9921d8c47610ab87c768e32d6ebfcbff4c
Kevin Muñoz <harquin09@gmail.com>
2023-11-22 16:28:48 -0500

    table

--- app/(front-end)/page.js
+++ app/(front-end)/page.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col min-h-screen">
-      <h2 className="text-4xl">Welcome to Limi Ecommerce</h2>
+      <h2 className="text-4xl">Welcome to VinKE Ecommerce</h2>
+      <h2>In Development</h2>
+      <Link href="/dashboard">
+        <button className="mt-2 p-4 bg-slate-600 text-slate-50 rounded-full">
+          Go to Dashboard (Only Front)
+        </button>
+      </Link>
     </div>
   );
 }
